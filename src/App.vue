6881c70d6886562fbe0d7a031d59c88a065b38ab
James Proett <jamesaproett@gmail.com>
2020-01-10 16:42:05 -0600

    setup

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <TopNav/>
+    <q-page-container>
+      <HelloWorld />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TopNav from './components/TopNav.vue'
+
+export default {
+  name: 'LayoutDefault',
+
+  components: {
+    HelloWorld,
+    TopNav
+  },
+
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/TopNav.vue
@@ -0,0 +1,37 @@
+<template>
+    <q-header elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar>
+        <q-toolbar-title class="toolbarTitle ">
+            <q-avatar>
+                <img src="../assets/cj.jpg">
+            </q-avatar>
+          Carly & James
+        </q-toolbar-title>
+      </q-toolbar>
+
+      <q-tabs align="left">
+        <q-route-tab to="/page1" label="Page One" />
+        <q-route-tab to="/page2" label="Page Two" />
+        <q-route-tab to="/page3" label="Page Three" />
+      </q-tabs>
+    </q-header>
+
+</template>
+<script>
+export default {
+  name: 'TopNav',
+  data() {
+      return {
+          'tab': ''
+      }
+  }
+}
+
+</script>
+
+<style>
+.toolbarTitle {
+    text-align: center;
+}
+
+</style>
